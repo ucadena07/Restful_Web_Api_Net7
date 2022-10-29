aed12e6a46ddd12ae5548d714cafee3cbc18b5fb
ucadena07 <ucadena07@gmail.com>
2022-10-29 18:29:31 -0500

    first api endpoint

---
+++ Controllers/VillaApiController.cs
@@ -0,0 +1,22 @@
+﻿using MagicVillaApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVillaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetVillas")]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>()
+            {
+                new Villa() {Id = 1, Name = "Villa 1"},
+                new Villa() {Id = 2, Name = "Villa 2"}
+            };
+        }
+    }
+}
---
+++ Models/Villa.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicVillaApi.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
