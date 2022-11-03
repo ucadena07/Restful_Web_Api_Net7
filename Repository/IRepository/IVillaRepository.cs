d2faa260136f872fafeac611ebdbf07010cd1546
ucadena07 <ucadena07@gmail.com>
2022-11-03 05:51:23 -0500

    Villa repository implementation

---
+++ Repository/IRepository/IVillaRepository.cs
@@ -0,0 +1,14 @@
+﻿using MagicVillaApi.Models;
+using System.Linq.Expressions;
+
+namespace MagicVillaApi.Repository.IRepository
+{
+    public interface IVillaRepository
+    {
+        Task Create(Villa entity);
+        Task Remove(Villa entity);
+        Task Save();
+        Task<List<Villa>> GetAll(Expression<Func<Villa>> filter = null);
+        Task<List<Villa>> Get(Expression<Func<Villa>> filter = null, bool tracked = true);
+    }
+}
---
+++ Repository/VillaRepository.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using MagicVillaApi.Data;
+using MagicVillaApi.Models;
+using MagicVillaApi.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace MagicVillaApi.Repository
+{
+    public class VillaRepository : IVillaRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+        public VillaRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+        public async Task Create(Villa entity)
+        {
+            await _db.Villas.AddAsync(entity);
+            await Save();
+        }
+
+        public async Task<Villa> Get(Expression<Func<Villa,bool>> filter = null, bool tracked = true)
+        {
+            IQueryable<Villa> query = _db.Villas;
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+
+            if (filter != null)
+                query = query.Where(filter);
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Villa>> GetAll(Expression<Func<Villa,bool>> filter = null)
+        {
+            IQueryable<Villa> query = _db.Villas;
+            if(filter != null)
+                query = query.Where(filter);
+            return await query.ToListAsync();
+
+        }
+
+        public async Task Remove(Villa entity)
+        {
+            _db.Remove(entity); 
+            await Save();   
+        }
+
+        public async Task Save()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
