26dfc8eb064b87470831663ccaee85fb3c5a9665
ucadena07 <ucadena07@gmail.com>
2022-11-05 09:08:54 -0500

    auto mapper in web app

---
+++ MagicVilla_Web/Mapping/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MagicVillaApi.Models;
+using MagicVillaApi.Models.Dtos;
+
+namespace MagicVillaApi.Mapping
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<Villa, VillaDto>().ReverseMap();
+            CreateMap<Villa,VillaCreateDto>().ReverseMap();
+            CreateMap<Villa,VillaUpdateDto>().ReverseMap();
+
+            CreateMap<VillaNumber, VillaNumberDTO>().ReverseMap();
+            CreateMap<VillaNumber, VillaNumberCreateDTO>().ReverseMap();
+            CreateMap<VillaNumber, VillaNumberUpdateDTO>().ReverseMap();
+        }
+    }
+}
