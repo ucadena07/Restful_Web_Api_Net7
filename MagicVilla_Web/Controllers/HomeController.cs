9c1669830b689ad514c4b65163050d54e56bfacf
ucadena07 <ucadena07@gmail.com>
2022-11-07 06:32:42 -0600

    villa home page

--- MagicVilla_Web/Controllers/HomeController.cs
+++ MagicVilla_Web/Controllers/HomeController.cs
@@ -1,32 +1,35 @@
-﻿using MagicVilla_Web.Models;
+﻿using AutoMapper;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dtos;
+using MagicVilla_Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace MagicVilla_Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+		private readonly IVillaService _villaService;
+		private readonly IMapper _mapper;
+		public HomeController(IVillaService villaService, IMapper mapper)
+		{
+			_villaService = villaService;
+			_mapper = mapper;
+		}
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public async Task<IActionResult> Index()
+		{
+			List<VillaDto> list = new();
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+			var response = await _villaService.GetAllAsync<APIResponse>();
+			if (response != null && response.IsSuccess)
+			{
+				list = JsonConvert.DeserializeObject<List<VillaDto>>(Convert.ToString(response.Result));
+			}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+			return View(list);
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
