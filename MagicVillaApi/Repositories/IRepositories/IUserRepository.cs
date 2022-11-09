fc3b9799c10dfb2690011ccba9a01d35a8a928e1
ucadena07 <ucadena07@gmail.com>
2022-11-09 06:00:21 -0600

    add user repository

---
+++ MagicVillaApi/Repositories/IRepositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using MagicVillaApi.Models;
+using MagicVillaApi.Models.Dtos;
+
+namespace MagicVillaApi.Repositories.IRepositories
+{
+    public interface IUserRepository
+    {
+        bool IsUniqueUser(string username); 
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+        Task<LocalUser> Register(RegistrationRequestDTO registrationRequestDto);
+    }
+}
---
+++ MagicVillaApi/Repositories/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using MagicVillaApi.Data;
+using MagicVillaApi.Models;
+using MagicVillaApi.Models.Dtos;
+using MagicVillaApi.Repositories.IRepositories;
+
+namespace MagicVillaApi.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+        public UserRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+
+        }
+        public bool IsUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LocalUser> Register(RegistrationRequestDTO registrationRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
