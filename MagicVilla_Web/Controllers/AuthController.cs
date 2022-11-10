2ea466175f5c62a4344e2ca9c715949d8430d008
ucadena07 <ucadena07@gmail.com>
2022-11-10 06:58:53 -0600

    auth controller and action methods

---
+++ MagicVilla_Web/Controllers/AuthController.cs
@@ -0,0 +1,60 @@
+﻿using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dtos;
+using MagicVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authservice;
+
+        public AuthController(IAuthService authservice)
+        {
+            _authservice = authservice;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDTO obj = new();
+            return View(obj);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginRequestDTO obj)
+        {
+            return View(obj);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationRequestDTO obj)
+        {
+            var resp = await _authservice.RegisterAsync<APIResponse>(obj);
+            if(resp is not null && resp.IsSuccess)
+            {
+                return RedirectToAction(nameof(Login));
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+    }
+}
