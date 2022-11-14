a9fbfd872322d7799306ba08c4b69ce8c39e90fe
ucadena07 <ucadena07@gmail.com>
2022-11-14 06:25:15 -0600

    setup identity server

---
+++ MagicVilla_Identity/SD.cs
@@ -0,0 +1,66 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace MagicVilla_Identity
+{
+    public static class SD
+    {
+        public const string Admin = "admin";
+        public const string Customer = "customer";
+
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope(name: "magic", displayName: "Magic Server")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                },
+                new Client
+                {
+                    ClientId = "magic",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "magic", IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Email,IdentityServerConstants.StandardScopes.Profile},
+                    RedirectUris = { "https://localhost:7002/signin-oidc" },
+                    PostLogoutRedirectUris = { "https://localhost:7002/signout-callback-oidc" }
+                }
+            };
+    }
+}
