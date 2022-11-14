d1d6b023afc65c5563d3034c7dc0d54cb16d39b5
ucadena07 <ucadena07@gmail.com>
2022-11-14 05:56:09 -0600

    configure identity server

--- MagicVilla_Identity/Program.cs
+++ MagicVilla_Identity/Program.cs
@@ -1,7 +1,35 @@
+using MagicVilla_Identity.Data;
+using MagicVilla_Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Add Db
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+//Add Identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+//Add Razor Pages
+builder.Services.AddRazorPages();
+
+//Add Identity Server 
+builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+
+});
 
 var app = builder.Build();
 
@@ -18,7 +46,13 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+//use identity server instead of app.useauthentication
+app.UseIdentityServer();
+
 app.UseAuthorization();
+
+//Add mapping to razor pages
+app.MapRazorPages().RequireAuthorization();
 
 app.MapControllerRoute(
     name: "default",
